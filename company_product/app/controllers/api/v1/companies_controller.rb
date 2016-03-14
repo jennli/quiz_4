b56d9a929cdd944fca228974f431b6193799db3d
Jennifer Li <ldnjennifer@hotmail.com>
2016-03-14 13:00:17 -0700

    bonus question 1 redo api with jbulder fix controller

--- company_product/app/controllers/api/v1/companies_controller.rb
+++ company_product/app/controllers/api/v1/companies_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::CompaniesController < ApplicationController
 
   def index
     @companies = Company.all
-    # render json: @companies.to_json(only: [:name])
+    render json: @companies, only: [:name, :products]
   end
 end
